324db1457f82dfcf9de5660589df1a17c3018d1c
John DiBaggio <jsantoku@gmail.com>
2014-07-24 12:15:18 -0500

    getting response from github authentication
    getting response from github authentication

--- js/authentication.js
+++ js/authentication.js
@@ -1,14 +1,74 @@
+// Set provider equal to github
+var provider = 'github';
+
 // Initialize with your OAuth.io app public key
 OAuth.initialize('X4CpCU8h86NObiHdtI1jOJ2YZM8');
 
 // callback_url is the URL where users are redirected
 // after being authorized
-//OAuth.redirect('github', 'https://oauth.io/auth');
+//OAuth.redirect(provider, 'https://oauth.io/auth');
 
-OAuth.popup('github', function(error, success){
+OAuth.popup(provider, function(error, result){
+  // handle error
+  if (error) {
+  	console.log(err);
+  	return;
+  }
   // See the result below
-  console.log(success);
+  // console.dir(result);
+
 });
+
+OAuth.popup(provider)
+.done(function(result) {
+    result.me()
+    .done(function (response) {
+    	console.dir(response);
+        // console.log('Username: ', response.alias);
+        // console.log('Name: ', response.name);
+        // console.log('Email: ', response.email);
+    })
+    .fail(function (err) {
+        //handle error with err
+    });
+})
+.fail(function (err) {
+    //handle error with err
+});
+
+
+//POST Request
+//Let's say the /message endpoint on the provider waits for
+//a POST request containing the fields "user_id" and "content"
+//and returns the field "id" containing the id of the sent message 
+// OAuth.popup(provider)
+// .done(function(result) {
+//     result.post('/message', {
+//         data: {
+//             user_id: 100,
+//             content: 'John'
+//         }
+//     })
+//     .done(function (response) {
+//         //this will display the id of the message in the console
+//         console.log(response.id);
+//     })
+//     .fail(function (err) {
+//         //handle error with err
+//     });
+// })
+// .fail(function (err) {
+//     //handle error with err
+// });
+
+// need to use GitHub API for posting to repository https://developer.github.com/v3/
+
+
+
+
+
+
+
 
 // OAuth.callback('github.com')
 // .done(function(result) {
@@ -23,16 +83,16 @@ OAuth.popup('github', function(error, success){
 
 
 // In callback URL
-// OAuth.callback('github', (error, success) { 
+// OAuth.callback(provider, (error, success) { 
 //   // See the result below
 //   console.dir(access_token);
 // });
 
-// OAuth.popup('github', function(error, success){
+// OAuth.popup(provider, function(error, success){
 //   // See the result below
 // });
 
-// OAuth.popup('github')
+// OAuth.popup(provider)
 // .done(function(result) {
 
 // })
@@ -50,7 +110,7 @@ OAuth.popup('github', function(error, success){
 // });
 
 
-// OAuth.popup('github', function(err, result) {
+// OAuth.popup(provider, function(err, result) {
 // 	if (err) {
 // 		console.log(err); // do something with error
 // 		return;
@@ -59,5 +119,3 @@ OAuth.popup('github', function(error, success){
 // 	console.log(result); // do something with result
 // });
 
-
-var provider = 'github';
