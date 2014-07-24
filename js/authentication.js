3b23e3c034b252e8d580e87215d14870ce76d455
John DiBaggio <jsantoku@gmail.com>
2014-07-24 01:11:06 -0500

    working on github authentication
    using OAuth.io service
    
    redirect vs popup?
    
    callback url: https://oauth.io/auth

--- js/authentication.js
+++ js/authentication.js
@@ -1,15 +1,44 @@
-OAuth.initialize('50d5b8dcdbb4d01a21e2');
+// Initialize with your OAuth.io app public key
+OAuth.initialize('X4CpCU8h86NObiHdtI1jOJ2YZM8');
+
+// callback_url is the URL where users are redirected
+// after being authorized
+//OAuth.redirect('github', 'https://oauth.io/auth');
+
+OAuth.popup('github', function(error, success){
+  // See the result below
+  console.log(success);
+});
+
+// OAuth.callback('github.com')
+// .done(function(result) {
+// 	console.dir(result);
+//     //use result.access_token in your API request
+//     //or use result.get|post|put|del|patch|me methods (see below)
+// })
+// .fail(function (err) {
+// 	console.dir(err);
+//     //handle error with err
+// });
+
+
+// In callback URL
+// OAuth.callback('github', (error, success) { 
+//   // See the result below
+//   console.dir(access_token);
+// });
+
+// OAuth.popup('github', function(error, success){
+//   // See the result below
+// });
 
 // OAuth.popup('github')
 // .done(function(result) {
-// console.log(result.access_token);
+
 // })
 // .fail(function(err) {
 // console.log(err);
 // });
-
-// OAuth.redirect('github', "callback/url");
-// OAuth.redirect('github', "http://localhost.dev/Dashboard_Gigabots_dibaggioj/callback");
 
 // OAuth.callback('github.com')
 // .done(function(result) {
