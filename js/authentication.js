01dfceb8ecbb6923e007a1721cff95799d0b2d66
John DiBaggio <jsantoku@gmail.com>
2014-07-28 18:51:48 -0500

    started adding user stuff to navbar
    user dropdown

--- js/authentication.js
+++ js/authentication.js
@@ -1,3 +1,54 @@
+$('#signIn').click(function() {
+	// Set provider equal to github
+	var provider = 'github';
+
+	// Initialize with your OAuth.io app public key
+	OAuth.initialize('X4CpCU8h86NObiHdtI1jOJ2YZM8');
+
+	// callback_url is the URL where users are redirected
+	// after being authorized
+	//OAuth.redirect(provider, 'https://oauth.io/auth');
+
+	OAuth.popup(provider, function(error, result){
+	  // handle error
+	  if (error) {
+	  	console.log(err);
+	  	return;
+	  }
+	  // See the result below
+	  // console.dir(result);
+
+	});
+
+	OAuth.popup(provider)
+	.done(function(result) {
+	    result.me()
+	    .done(function (response) {
+	    	console.dir(response);
+	        // console.log('Username: ', response.alias);
+	        // console.log('Name: ', response.name);
+	        // console.log('Email: ', response.email);
+	    })
+	    .fail(function (err) {
+	        //handle error with err
+	    });
+	})
+	.fail(function (err) {
+	    //handle error with err
+	});
+
+    var parent = document.getElementById( "navbarLinks" );
+    var child = document.getElementById( "signIn" );
+    parent.removeChild( child ); // remove sign in link
+    var para = document.createElement( "li" ); // create list element
+    var node = document.createTextNode( response.name );
+    para.setAttribute( "class", "dropdown" );
+    para.setAttribute( "id", "userDropdown" );
+    para.appendChild( node );
+    parent.appendChild( para );
+
+});
+
 // Set provider equal to github
 var provider = 'github';
 
@@ -27,7 +78,69 @@ OAuth.popup(provider)
         // console.log('Username: ', response.alias);
         // console.log('Name: ', response.name);
         // console.log('Email: ', response.email);
-    })
+    	var parent = document.getElementById( "navbarLinks" );
+	    var child = document.getElementById( "signIn" );
+	    parent.removeChild( child ); // remove sign in link
+	    var para = document.createElement( "li" ); // create list element
+	    para.setAttribute( "class", "dropdown" );
+
+	    var para2 = document.createElement( "a" );
+	    para2.setAttribute( "class", "dropdown-toggle" );
+	    para2.setAttribute( "data-toggle", "dropdown" );
+	    para2.setAttribute( "id", "userDropdown" );
+	    para2.setAttribute( "title", "User sign-in and sign-out" );
+	    var node = document.createTextNode( response.name + " " );
+	    para2.appendChild( node );
+
+	    var para0 = document.createElement( "span" );
+	    para0.setAttribute("class", "caret");
+	    para2.appendChild( para0 );
+
+	    var para3 = document.createElement( "ul" );
+	    para3.setAttribute( "class", "dropdown-menu" );
+	    para3.setAttribute( "id", "userDropdownList" );
+
+	    var para4 = document.createElement( "li" );
+
+	   	var para5 = document.createElement( "a" );
+	   	para5.setAttribute( "id", "userSignOut" );
+	   	var node5 = document.createTextNode( "Sign out" );
+	    para5.appendChild(node5);
+	    
+	    para4.appendChild(para5);
+
+	    var para6 = document.createElement( "li" );
+
+	    var para7 = document.createElement( "a" );
+	    para7.setAttribute( "id", "userGithub" );
+	    para7.setAttribute( "title", "Visit my GitHub profile" );
+	    var githubUserUrl = "https://github.com/" + response.alias;
+	    para7.setAttribute( "href", githubUserUrl );
+	    para7.setAttribute( "target", "_blank" );
+	    var node7 = document.createTextNode( "My GitHub Profile" );
+	    para7.appendChild(node7);
+	    para6.appendChild(para7);
+	    para4.insertBefore(para6,para5);
+
+	   	para3.appendChild(para4);
+
+	   	para.appendChild( para3 );
+
+	    para.appendChild( para2 );
+	    var directions = document.getElementById( "directions" );
+	    parent.insertBefore(para,directions);
+	    //parent.appendChild( para );
+	    })
+
+	   //    	<li class="dropdown">
+				// <a class="dropdown-toggle" id="botSelector" data-toggle="dropdown" title="Control a Currently Connected Gigabot">Select Gigabot <span class="caret"></span></a>
+				// <ul class="dropdown-menu" id="botSelectorList">
+	   //          <li class="divider" id="dropdownDivider"></li>
+	   //          <li><a id="addBot">Add a New Bot</a></li>
+		  //       </ul>
+	   //    	</li>
+
+
     .fail(function (err) {
         //handle error with err
     });
@@ -35,6 +148,8 @@ OAuth.popup(provider)
 .fail(function (err) {
     //handle error with err
 });
+
+
 
 
 //POST Request
